23f86accaf51976d3db04c9025cd073580879e9a
B09S304est <B09S304est@MEDAULAS.UPB>
2026-07-28 13:21:39 -0500

    mi primer commit menol

---
+++ Variables_constantes1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Variables_constantes1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "panchocraft18";
+            Console.WriteLine ("hola  " + nombre + " menol ke lo ke ");
+        }
+    }
+}
