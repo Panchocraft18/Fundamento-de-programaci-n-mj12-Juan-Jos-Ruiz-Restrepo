7b3abc07ab04002543aabf379463d87529c27768
Panchocraft18 <95972880+Panchocraft18@users.noreply.github.com>
2026-08-13 13:25:10 -0500

    condicionales multiples
    ke lo que menor hablaemlo papi

---
+++ 6.condicionalesMultiples/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.condicionalesMultiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Condicionales Múltiples
+            // crear un algoritmo que muester x pantalla 5 opciones y un mensaje que diga la opcion seleccionada 
+
+            int respuesta = 0;
+            Console.WriteLine("--------------Menu--------------");
+            Console.WriteLine("1. Opción 1               2.Opción 2");
+            Console.WriteLine("3. Opción 3               4.Opción 4");
+            Console.WriteLine("5. Opción 5");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Eliga uno de nuestros menus");
+            respuesta=int.Parse(Console.ReadLine());
+
+            switch (respuesta)
+            {
+                case 1:
+                    Console.WriteLine("Usted eligió la opción 1");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Uestese eligió la opción 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Usted eligió la opción 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Usted eligió la opción 4");
+                    break;
+                    case 5:
+                    Console.WriteLine("Usted eligió la opción 5");
+                    break;
+                    default:
+                    Console.WriteLine("Elige una opción válida");
+                    break;
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ 6.condicionalesMultiples/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("6.condicionalesMultiples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("6.condicionalesMultiples")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("3b52332e-8d7a-4026-9bee-ca359ef5dd3b")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
