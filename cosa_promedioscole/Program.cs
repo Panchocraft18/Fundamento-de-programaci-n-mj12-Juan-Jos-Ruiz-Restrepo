d6cab4492a196510e2ffd84253f31ef736d502ba
Panchocraft18 <95972880+Panchocraft18@users.noreply.github.com>
2026-08-22 18:51:03 -0500

    calculador de promedio de notas

---
+++ cosa_promedioscole/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+namespace cosa_promedioscole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double finalProm;
+            string state = "Error";
+
+            Console.Write("Ingrese el promedio final del alumno: ");
+            finalProm = double.Parse(Console.ReadLine());
+
+            if (finalProm >= 0)
+            {
+                if (finalProm <= 5)
+                {
+                    state = "Desaprobado";
+                }
+                else
+                {
+                    if (finalProm <= 10)
+                    {
+                        state = "Vacacional";
+                    }
+                    else
+                    {
+                        if (finalProm <= 15)
+                        {
+                            state = "Requiere mejorar";
+                        }
+                        else
+                        {
+                            if (finalProm <= 20)
+                            {
+                                state = "Supera las expectativas";
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ingrese un promedio correcto.");
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ingrese un promedio correcto.");
+            }
+
+            Console.WriteLine("El estado del alumno es: " + state);
+        }
+    }
+}
