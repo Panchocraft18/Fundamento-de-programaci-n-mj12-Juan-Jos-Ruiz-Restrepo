ad442135f9d98e1a24d71ffa152b74f7e26d21c9
Panchocraft18 <95972880+Panchocraft18@users.noreply.github.com>
2026-08-25 13:30:05 -0500

    factorial de un numero

---
+++ 7.ciclo while/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.ciclo_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          //  int contador = 1;
+           // int acomulador = 1;
+
+            //while (contador < 5)
+            //{
+              //  contador        ++;
+                //acomulador  = acomulador + contador;
+            //}
+            //Console.WriteLine($"La suma de los 5 primeros números es :  { acomulador}");
+            int contador = 1;
+            int acomulador = 1;
+            int numero = 1;
+            ;
+            Console.WriteLine("Ingrese un número: ");
+            numero= int.Parse(Console.ReadLine());
+            while (contador < numero )
+            {
+                contador++;
+             acomulador = acomulador * contador;
+            }
+            Console.WriteLine($"El resultado de la multiplicación es:" + acomulador );
+        }
+    }
+}
