39bed3e6a19ac116e911ae75f0a39dc38516fa87
Panchocraft18 <95972880+Panchocraft18@users.noreply.github.com>
2026-08-27 13:34:01 -0500

    whilw contadores+ condicionales

---
+++ 9.1While_final_clase/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1While_final_clase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Realizar un algoritmo que le permita al ususario infresr por teclnado numero enteros al alzar. //Al usuaerio el corresponder ingresar la cantidas de numero qeu va a introcurir // mostar numero mayores a 0, menores a 0 y iguales a 0*//*
+
+            int numeros = 0;
+            int cantidad, mayores = 0, menores = 0, iguales = 0;
+
+            Console.WriteLine("Ingrese la cantidad de numeros que desea ingresar: ");
+            cantidad = Convert.ToInt32(Console.ReadLine());
+
+            int contador = 1;
+
+            while (contador <= cantidad)
+            {
+                Console.WriteLine("Ingrese el número " + contador);
+                numeros = Convert.ToInt32(Console.ReadLine());
+
+                if (numeros > 0)
+                {
+                    mayores++;
+                }
+                else if (numeros < 0)
+                {
+                    menores++;
+                }
+                else
+                {
+                    iguales++;
+                }
+
+                contador++;
+            }
+
+            Console.WriteLine("Mayores a 0: " + mayores);
+            Console.WriteLine("Menores a 0: " + menores);
+            Console.WriteLine("Iguales a 0: " + iguales);+*/
+
+
+             //Escribir yn algoritmo que cuente cuantos numeros hay pares en ese rango de numero del 1 al numero n (operador mod en c#)
+                int numeropares = 1;
+            int numero_usuario = 0;
+            int numero = 0;
+
+            Console.WriteLine("Ingrese la cantidad de numeros que desea evaluar si son par : ");
+            numero_usuario = int.Parse(Console.ReadLine());
+            while (numeropares <= numero_usuario)
+            {
+                if (numeropares % 2 == 0)
+                {
+                    numero++;
+                }
+                numeropares++;
+            }
+            Console.WriteLine("La cantidad de numeros pares es: " + numero);
+        }
+    }
+}
---
+++ 9.1While_final_clase/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("9.1While_final_clase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("9.1While_final_clase")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("3a4856ab-f025-444a-8ea5-8929f93db2ec")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
