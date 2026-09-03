3cb855a61cab3f0399f3109b4130fc0515dae263
Panchocraft18 <95972880+Panchocraft18@users.noreply.github.com>
2026-09-03 13:11:37 -0500

    ciclo de whiles en mientras

---
+++ 11.Ciclo_doWhile/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+
+namespace _11.Ciclo_doWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ciclo do while
+            int contador = 1;
+            int acomulador = 0;
+
+            do
+            {
+                acomulador += contador;
+                contador++;
+            } while (contador <= 10);
+
+            Console.WriteLine($"La suma de los cicnco primeros números enteros es:  {acomulador}");*/
+
+            // algortimo un número y genere su correspodiente tabla de multiplica desde el 1 hastas el 10 asi sucesivamente hastas que el usuario ya no desee continuar generando tablas de multiplicar
+
+            /*int contador = 1;
+
+            int numero;
+            char continuar = ' ';
+            do
+            {
+
+
+                Console.WriteLine("Ingrese un número para generar su tabla de multiplicar: ");
+                numero = int.Parse(Console.ReadLine());
+                do
+                {
+                    Console.WriteLine($"el resultado es{numero}x{contador}={numero * contador}");
+
+                    contador++;
+                    
+                } while (contador <= 10);
+                contador = 1;
+                Console.WriteLine("Desea generar otra tabla de multiplicar? (Y/N)");
+                continuar = char.Parse(Console.ReadLine());
+            } while (continuar == 'y');*/
+
+            //Crear un algortimo que permita calcular y mostrar por pantalla los números primos hastas el número 1007
+            int numero = 0;
+            Console.WriteLine("Ingrese lel numero hasta el cual desea calcular los números primos: ");
+             numero = int.Parse(Console.ReadLine());
+
+            int contador = 1;
+            int cantidadPares = 0;
+
+            while (contador <= numero)
+            {
+                if (contador % 2 == 0)
+                {
+                    cantidadPares++;
+                }
+
+                contador++;
+            }
+        }
+    }
+ }
+    
+
---
+++ 12.ciclo do wiles/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.ciclo_do_wiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Algoritmo que le pida a un grupo de usuarios su nimbre número de cuenta y slado ; oara cada usuarios se debe hacer lo siguiente
+            si el salfo es mayor o iugla a 3,000.000 sacar ub mensaje que diga el nombre del usuario el número de cuanta el saldo y "es apto para el crediot 
+
+            si el salfo es menor a 3,000.000 sacar un mensaje que diga el nombre del usuario el número de cuenta el saldo y "no es apto para el credito"
+                El algoritmo debe prermintir pregutnat la infromacion de los usuarios hasta que se le indique que ta no se desea preguntar mas 
+            ademas, el algortimo debe permitir mostar el número de usuarios a los que se le pregunto la informacion y deme mostar el promedio de los saldos  */
+            int usuarios = 0;
+           
+            int saldo = 0;
+            int promedio = 0;
+            string nombre;
+            bool continuar = true;
+            string respuesta;
+
+            do
+            {
+
+                Console.WriteLine("Ingrese su nombre del usuario: ");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese su número de cuenta : ");
+                int numeroCuenta = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese su saldo : ");
+                saldo = int.Parse(Console.ReadLine());
+                if (saldo >= 3000000)
+                {
+                    Console.WriteLine($"el usuario{nombre} con el número de cuenta {numeroCuenta} con un saldo de {saldo} es apto para el crédito");
+                }
+                else
+                {
+                    Console.WriteLine($"el usuario{nombre} con el número de cuenta {numeroCuenta} con un saldo de {saldo} no es apto para el crédito");
+                   
+                    usuarios++;
+                }
+                Console.WriteLine("Desea volver a verificar si credito si/no");
+                respuesta = Console.ReadLine();
+                if (respuesta == "no")
+                {
+                    continuar = false;
+                    promedio = saldo / usuarios;
+                    Console.WriteLine($"El número de usuarios a los que se le pregunto la información es {usuarios} y el promedio de los saldos es {promedio}");
+                }
+                
+             }while (continuar);
+        }
+    }
+}
